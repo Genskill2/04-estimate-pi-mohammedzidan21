70d196f1d0d265eea1148522617b29f47adfa407
mohammedzidan21 <84699952+mohammedzidan21@users.noreply.github.com>
2021-06-02 13:18:58 +0530

    Updated wllis.c

--- wallis.c
+++ wallis.c
@@ -3,24 +3,15 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int);
-
-int main(void) {
-  float pi;
-  for (int i=0; i<5; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      abort();
-    }
-  }
-
-  for (int i=500; i<3000; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-      abort();
-    }
-  }
+float wallis_pi(int n)
+{
+  float p=1;
+  for(int i=0; i<=n;i++)
+    p*=(4*n*n)/((4*n*n)+1);
+  return p;
 }
-
+int main(void)
+{
+  int x = get_int("Enter number of terms");
+  printf("%d \n",wallis_pi(x));
+}
